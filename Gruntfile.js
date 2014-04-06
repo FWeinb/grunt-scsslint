5f338896b3122d6b3e4610e229d5646659f52cd6
Fabrice Weinberg <Fabrice@weinberg.me>
2014-04-06 14:15:27 +0200

    replaced wrong path

--- Gruntfile.js
+++ Gruntfile.js
@@ -25,7 +25,7 @@ module.exports = function (grunt) {
     },
     scsslint: {
       defaultOptions: {
-        src: ['../SassyJson/stylesheets/**/*'],
+        src: ['./test/**/*'],
       }
     },
 
